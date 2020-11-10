29799f0d750ce95345ec8124c64d7a478f1ca964
Zero <zeroa5620@gmail.com>
2020-11-10 09:45:01 +0545

    parsing

--- precommit.cpp
+++ precommit.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 #define popen _popen
@@ -13,6 +14,7 @@ const char *CMD = "git diff --cached";
 typedef unsigned int uint;
 
 #define static_count(a) (sizeof(a) / sizeof((a)[0]))
+#define string_lit(s) String { (uint)(sizeof(s) - 1), s }
 
 struct String {
     uint count;
@@ -47,6 +49,57 @@ FILE *get_temporary_file() {
     return fp;
 }
 
+struct Trav {
+    String string;
+    uint current;
+};
+
+bool trav_continue(Trav *t) {
+    return t->current < t->string.count;
+}
+
+String trav_get_line(Trav *t) {
+    String line;
+    line.data = t->string.data + t->current;
+    line.count = 0;
+    
+    char *p = t->string.data + t->current;
+    
+    while (*p) {
+        if (*p == '\n' || *p == '\r') {
+            if (*p == '\r' && *(p + 1) == '\n') {
+                t->current += 1;
+                break;
+            }
+            t->current += 1;
+        }
+        
+        line.count += 1;
+        p += 1;
+        t->current += 1;
+    }
+    
+    return line;
+}
+
+bool string_starts_with(String a, String b) {
+    if (a.count < b.count) return false;
+    
+	for (uint i = 0; i < b.count; ++i) {
+		if (a.data[i] != b.data[i]) return false;
+	}
+    
+	return true;
+}
+
+void string_print(String a) {
+    char *p = a.data;
+	for (uint i = 0; i < a.count; ++i) {
+        putchar(*p);
+        p += 1;
+	}
+}
+
 int main() {
     FILE* tmpf = get_temporary_file();
     
@@ -67,9 +120,21 @@ int main() {
     
     String string = read_entire_file(tmpf);
     
+    Trav t;
+    t.string = string;
+    t.current = 0;
+    
     printf("== CONTENT ==\n");
-    printf("%s\n", string.data);
+    
+    while (trav_continue(&t)) {
+        String line = trav_get_line(&t);
+        if (string_starts_with(line, string_lit("+++"))) {
+            string_print(line);
+        }
+    }
+    
     printf("== CONTENT ==\n");
+    
     
     return 0;
 }
