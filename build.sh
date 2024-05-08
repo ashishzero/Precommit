6b57302b5962d5f3d454b826960914c47373f502
Ashish Bhattarai <zeroa5620@gmail.com>
2024-05-08 21:31:20 +0900

    append to install.sh

--- build.sh
+++ build.sh
@@ -16,13 +16,13 @@ $CC $compilerFlags $sourceFiles -o bin/$nameOfExe
 
 echo 'Generating installer...'
 rm -f install.sh
-echo '#!/bin/sh' > install.sh
-echo 'if [ ! -d "./.git/hooks" ]; then' > install.sh
-echo '  echo Not a git root directory' > install.sh
-echo '  exit 1' > install.sh
-echo 'fi' > install.sh
-echo 'echo "Installing pre-commit..."' > install.sh
-echo 'echo cp -rf ""$pwd/bin/pre-commit"" ./.git/hooks/' > install.sh
-echo 'echo "Installed."' > install.sh
+echo '#!/bin/sh' >> install.sh
+echo 'if [ ! -d "./.git/hooks" ]; then' >> install.sh
+echo '  echo Not a git root directory' >> install.sh
+echo '  exit 1' >> install.sh
+echo 'fi' >> install.sh
+echo 'echo "Installing pre-commit..."' >> install.sh
+echo 'echo cp -rf ""$pwd/bin/pre-commit"" ./.git/hooks/' >> install.sh
+echo 'echo "Installed."' >> install.sh
 
 echo 'Finished.'
